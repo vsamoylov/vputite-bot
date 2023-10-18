9bce4c248792b663bd91462d194d82322b0fc9d9
vsamoylov <vsamoylov@griddynamics.com>
2023-10-18 18:44:33 +0400

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,24 @@
+﻿class strings:
+
+    # ------------------------ System status -----------------------------
+    RETURNED = "Returned"
+    SUBMITTED = "Submitted"
+    FAILED = "Failed"
+    BYE = "Bye"
+    HELLO = "Hello"
+    OK = "Ok"
+    NICETRY = "Nice try!"
+
+
+    # ------------------------ User's Text -----------------------------
+    APPROVE = "Подтвердить" 
+    REJECT = "Отказать"
+    BOT_DESCRIPTION_TXT = "Через этот бот можно присылать фото и видео для публикации в канале @vputite"
+    WELLCOME_TXT = "Здравствуйте %name%! На связи с вами для формирования актуальной информации о ситуации в общественном транспорте"
+    PHOTO_REQUEST_TXT = "Пришлите пожалуйста фотографию"	
+    COMMENT_REQUEST_TXT = "Пришлите пожалуйста описание"
+    THANKS_TXT = "Спасибо за информацию, после одобрения модератором, она будет размещена в канале"
+    SUBSCRIBE_TXT = "Подписаться на канал"
+    SENDINFO_TXT = "Прислать информацию"
+    SUBSCRIBE_LINK_TXT = "<a href='https://t.me/+asHAHC92HK0wYzIy'>Подписаться на канал</a>"   # parse_mode = ParseMode.HTML
+    SENDINFO_LINK_TXT = "<a href='https://t.me/vputite_reportbot'>Прислать информацию</a>" # parse_mode = ParseMode.HTML
