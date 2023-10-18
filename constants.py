a739a7041db1eb1237e0683e493f0e3a9cc3667c
vsamoylov <vsamoylov@griddynamics.com>
2023-10-18 21:48:35 +0400

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,24 +1,11 @@
-class strings:
-
-    # ------------------------ System status -----------------------------
-    RETURNED = "Returned"
-    SUBMITTED = "Submitted"
-    FAILED = "Failed"
-    BYE = "Bye"
-    HELLO = "Hello"
-    OK = "Ok"
-    NICETRY = "Nice try!"
-
-
-    # ------------------------ User's Text -----------------------------
-    APPROVE = "Подтвердить" 
-    REJECT = "Отказать"
-    BOT_DESCRIPTION_TXT = "Через этот бот можно присылать фото и видео для публикации в канале @vputite"
-    WELLCOME_TXT = "Здравствуйте %name%! На связи с вами для формирования актуальной информации о ситуации в общественном транспорте"
-    PHOTO_REQUEST_TXT = "Пришлите пожалуйста фотографию"	
-    COMMENT_REQUEST_TXT = "Пришлите пожалуйста описание"
-    THANKS_TXT = "Спасибо за информацию, после одобрения модератором, она будет размещена в канале"
-    SUBSCRIBE_TXT = "Подписаться на канал"
-    SENDINFO_TXT = "Прислать информацию"
-    SUBSCRIBE_LINK_TXT = "<a href='https://t.me/+asHAHC92HK0wYzIy'>Подписаться на канал</a>"   # parse_mode = ParseMode.HTML
-    SENDINFO_LINK_TXT = "<a href='https://t.me/vputite_reportbot'>Прислать информацию</a>" # parse_mode = ParseMode.HTML
+TEXT_APPROVE = "Подтвердить"
+TEXT_REJECT = "Отказать"
+TEXT_BOT_DESCRIPTION = "Через этот бот можно присылать фото и видео для публикации в канале @vputite"
+TEXT_WELCOME = "Здравствуйте %name%! На связи с вами для формирования актуальной информации о ситуации в общественном транспорте"
+TEXT_PHOTO_REQUEST = "Пришлите пожалуйста фотографию"	
+TEXT_COMMENT_REQUEST = "Пришлите пожалуйста описание"
+TEXT_THANKS = "Спасибо за информацию, после одобрения модератором, она будет размещена в канале"
+TEXT_SUBSCRIBE = "Подписаться на канал"
+TEXT_SENDINFO = "Прислать информацию"
+TEXT_SUBSCRIBE_LINK = "<a href='https://t.me/+asHAHC92HK0wYzIy'>Подписаться на канал</a>"   # parse_mode = ParseMode.HTML
+TEXT_SENDINFO_LINK = "<a href='https://t.me/vputite_reportbot'>Прислать информацию</a>" # parse_mode = ParseMode.HTML
