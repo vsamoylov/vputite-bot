de63a904039375276b0ce01b537ba12f36df2278
vsamoylov <vsamoylov@griddynamics.com>
2023-10-18 22:53:31 +0400

    Update constants.py

--- constants.py
+++ constants.py
@@ -8,5 +8,5 @@ TEXT_COMMENT_REQUEST = "Пришлите пожалуйста описание"
 TEXT_THANKS = "Спасибо за информацию, после одобрения модератором, она будет размещена в канале"
 TEXT_SUBSCRIBE = "Подписаться на канал"
 TEXT_SENDINFO = "Прислать информацию"
-TEXT_SUBSCRIBE_LINK = "<a href='https://t.me/+asHAHC92HK0wYzIy'>Подписаться на канал</a>"   # parse_mode = ParseMode.HTML
-TEXT_SENDINFO_LINK = "<a href='https://t.me/vputite_reportbot'>Прислать информацию</a>" # parse_mode = ParseMode.HTML
+HTML_SUBSCRIBE_LINK = "<a href='https://t.me/+asHAHC92HK0wYzIy'>Подписаться на канал</a>"   # parse_mode = ParseMode.HTML
+HTML_SENDINFO_LINK = "<a href='https://t.me/vputite_reportbot'>Прислать информацию</a>" # parse_mode = ParseMode.HTML
