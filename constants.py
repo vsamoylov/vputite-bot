801c58be4edcaf6a5e89e28f9c8437b9025bd7f5
vsamoylov <vsamoylov@griddynamics.com>
2023-10-21 11:31:30 +0400

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,12 +1,19 @@
 TEXT_APPROVE = "Подтвердить"
 TEXT_REJECT = "Отказать"
+TEXT_LINKS = "+ ссылки"
 TEXT_BOT_DESCRIPTION = "Через этот бот можно присылать фото и видео для публикации в канале @vputite"
+TEXT_HELP="Помощь"
 TEXT_WELCOME = "Здравствуйте"
 TEXT_WELCOME_DESCRIPTION = "На связи с вами для формирования актуальной информации о ситуации в общественном транспорте"
 TEXT_PHOTO_REQUEST = "Пришлите пожалуйста фотографию"
 TEXT_COMMENT_REQUEST = "Пришлите пожалуйста описание"
 TEXT_THANKS = "Спасибо за информацию, после одобрения модератором, она будет размещена в канале"
+TEXT_USER_REJECT_CONFIRMATION = "К сожалению модераторы отклонили публикацию"
+TEXT_USER_APPROVE_CONFIRMATION = "Сообщение отправлено в канал"
+TEXT_ADMIN_REJECT_CONFIRMATION = "Отклонено администратором"
+TEXT_ADMIN_APPROVE_CONFIRMATION = "Одобрено администратором"
 TEXT_SUBSCRIBE = "Подписаться на канал"
 TEXT_SENDINFO = "Прислать информацию"
 HTML_SUBSCRIBE_LINK = "<a href='https://t.me/+asHAHC92HK0wYzIy'>Подписаться на канал</a>"   # parse_mode = ParseMode.HTML
 HTML_SENDINFO_LINK = "<a href='https://t.me/vputite_reportbot'>Прислать информацию</a>" # parse_mode = ParseMode.HTML
+HTML_INFO = HTML_SUBSCRIBE_LINK + "\n" + HTML_SENDINFO_LINK # parse_mode = ParseMode.HTML
